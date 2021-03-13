2c983b40b5bd9698a5b328046ac1966ff50d0220
technafo <uveys.cengiz@gmail.com>
2021-03-14 01:06:25 +0300

    Errare humanum est.

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/technafo/candylist/mono/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/technafo/candylist/mono/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/technafo/candylist/mono/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/technafo/candylist/mono/node_modules/infima/dist/css/default/default.css"),
-  require("/home/technafo/candylist/mono/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/technafo/candylist/mono/packages/documentation/src/css/custom.css"),
-];
