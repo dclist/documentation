a36b418e38d947411020f46b024b252da5810cb6
technafo <uveys.cengiz@gmail.com>
2021-03-31 11:40:17 +0300

    feat: Initial Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,160 @@
+module.exports = {
+  title: 'DCList Documentation',
+  tagline: 'Documentation of DCList.net',
+  url: 'https://docs.dclist.net/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'dclist',
+  projectName: 'documentation',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'tr'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+      },
+      tr: {
+        label: 'Türkçe',
+      },
+    },
+  },
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+    },
+    prism: {
+      theme: require('prism-react-renderer/themes/vsDark'),
+    },
+    navbar: {
+      title: 'DCList',
+      logo: {
+        alt: 'DCList Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://dclist.net',
+          label: 'Website',
+          position: 'left',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/dclist/documentation',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Site Documentation',
+              to: '/',
+            }
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.com/invite/XEKUMSVBRa',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/dclist',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/dclist/docs/edit/master/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: process.env.NODE_ENV === 'development',
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json',
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: '#5eb1d4',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#113240',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/logo.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/logo.png',
+            color: '#5eb1d4',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/logo.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#113240',
+          },
+        ],
+      },
+    ],
+  ]
+};
---
+++ sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mainSidebar: {
+        Introduction: ['introduction/greetings', 'introduction/features'],
+        Guides: ['guides/charts'],
+        SDKs: [
+            {
+                'Node SDK': [
+                    'sdk/node/home',
+                    {
+                        API: ['sdk/node/api/client'],
+                        Guides: ['sdk/node/guides/common']
+                    },
+                ],
+            },
+        ],
+    },
+}
